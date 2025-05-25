50de20547a94e668f17a7036c63707ecac12ca38
sandy <sandydwi75@gmail.com>
2025-05-25 22:29:01 +0700

    feat: add remote dashboard module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      loadRemoteModule('dashboard-module', './Component')
+        .then((m) => m.AppComponent),
+  }
+];
