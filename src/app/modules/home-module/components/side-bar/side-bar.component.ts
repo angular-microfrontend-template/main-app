76183ad15edfffcf3fd1a6c38e009daf8dda9e89
sandy <sandydwi75@gmail.com>
2025-05-26 22:09:33 +0700

    feat: restyle sidebar

--- src/app/modules/home-module/components/side-bar/side-bar.component.ts
+++ src/app/modules/home-module/components/side-bar/side-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -15,18 +15,45 @@ export class SideBarComponent {
     icon: string;
     label: string;
     url: string;
+    children: {
+      id: number;
+      icon: string;
+      label: string;
+      url: string;
+    }[];
   }[] = [
       {
         id: 1,
         icon: 'assets/images/icons/svg/dashboard_24px_outlined_white.svg',
         label: 'Dashboard',
-        url: '/admin/dashboard'
+        url: '/admin/dashboard',
+        children: []
       },
       {
         id: 2,
+        icon: 'assets/images/icons/svg/assessment_24px_outlined_white.svg',
+        label: 'Report',
+        url: '/admin/report',
+        children: []
+      },
+      {
+        id: 3,
         icon: 'assets/images/icons/svg/assignment_ind_24px_outlined_white.svg',
         label: 'User Management',
-        url: '/admin/user-management'
+        url: '/admin/user-management',
+        children: []
       },
     ]
+
+  constructor(
+    private readonly router: Router
+  ) { }
+
+  isRouteActive(route: string): boolean {
+    return Boolean(this.router.url == route);
+  }
+
+  goToPage(url: string): void {
+    this.router.navigate([url]);
+  }
 }
